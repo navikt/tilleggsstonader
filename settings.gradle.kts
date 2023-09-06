63b224b95d224340902b70d365aa77fe0adfad7f
Johan Blomgren <blommish@users.noreply.github.com>
2023-09-06 07:37:19 +0200

    Lagt til sak-frontend

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,6 @@ rootProject.name = "tilleggsstonader"
 
 includeBuild("tilleggsstonader-libs")
 includeBuild("tilleggsstonader-sak")
+includeBuild("tilleggsstonader-sak-frontend")
 includeBuild("tilleggsstonader-soknad")
 includeBuild("tilleggsstonader-soknad-api")
