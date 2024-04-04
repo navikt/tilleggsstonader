36534b4477756fca518b8992dc1b5cbed392af0b
Johan Blomgren <blommish@users.noreply.github.com>
2024-04-04 17:46:29 +0200

    Legger til jitpack til repos for å kunne laste ned sources

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ repositories {
     mavenCentral()
     mavenLocal()
 
+    maven("https://jitpack.io")
+
     maven {
         url = uri("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
     }
